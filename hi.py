a9d72c51f22a265df68bb25eeb29751f3a21ad65
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-10-31 16:31:18 +0000

    Tue, Oct 31, 2023, 4:31 PM +00:00

--- hi.py
+++ hi.py
@@ -1,4 +1,6 @@
 # ask for name, remove whitespace, capitalize
 name = input("what's your name? ").strip().title()
+#split to first and last
+first,last = name.split(" ")
 #print output
-print(f"hello,{name}")
+print(f"hello,{first}")

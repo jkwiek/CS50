a8d437b87c5efba98d5d653760dee0d786637ce4
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-10-31 16:26:33 +0000

    Tue, Oct 31, 2023, 4:26 PM +00:00

--- hi.py
+++ hi.py
@@ -1,7 +1,5 @@
 # ask for name
-name = input("what's your name? ")
-#remove whitespace from str
-name = name.strip()
-name = name.title()
+name = input("what's your name? ").strip().title()
+#remove whitespace from str and capitalize name
 #print output
 print(f"hello,{name}")

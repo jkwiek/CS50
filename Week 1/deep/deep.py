4eea9141ad060e8d703841b24b83bc38efec5737
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-03 22:17:40 +0000

    Fri, Nov 3, 2023, 5:17 PM -05:00

---
+++ Week 1/deep/deep.py
@@ -0,0 +1,5 @@
+input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+if input == "42" | input.lower() == "forty-two" | input.lower() == "forty two"
+    print("Yes")
+else
+    print("No")

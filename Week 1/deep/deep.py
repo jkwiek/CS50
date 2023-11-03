b955dd2812461c780bd5746a9e7cd3224ab79130
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-03 22:31:40 +0000

    Fri, Nov 3, 2023, 5:31 PM -05:00

--- Week 1/deep/deep.py
+++ Week 1/deep/deep.py
@@ -1,12 +1,6 @@
 Answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
-if Answer == "42":
+Answer = Answer.strip().lower().replace("-"," ")
+if Answer == "42" or Answer == "forty two":
     print("Yes")
-if (Answer = answer.strip().lower().replace(-, )) == forty two:
-    print("Yes")
-if
-match Answer:
-    case "42" | "forty-two" | "forty two":
-        print("Yes")
-    case _:
-        print("No")
-if
+else:
+    print("No")

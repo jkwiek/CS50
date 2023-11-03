4bb731a889b4b5ad286bc39b5d41814edfe38119
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-03 22:08:12 +0000

    Fri, Nov 3, 2023, 5:08 PM -05:00

--- house.py
+++ house.py
@@ -1,3 +1,12 @@
 name = input("What's your name? ")
-if name == "Harry":
-    print
+match name:
+    case "Harry" | "Hermione" | "Ron":
+        print ("Gryffindor")
+    case "Hermione":
+        print ("Gryffindor")
+    case "Ron":
+        print ("Gryffindor")
+    case "Draco":
+        print ("Gryffindor")
+    case _:
+        print ("Who?")

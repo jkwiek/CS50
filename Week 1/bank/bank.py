9e84fca1b79d3f5e66131f9c7ad67ffee41f78d5
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-03 23:03:55 +0000

    Fri, Nov 3, 2023, 6:03 PM -05:00

--- Week 1/bank/bank.py
+++ Week 1/bank/bank.py
@@ -1,7 +1,7 @@
 greeting = input("Greeting: ").strip().lower()
 if greeting.startswith("hello") == True:
     print("$0")
-if greeting.startswith("hello") == False and greeting.startswith("h"):
+elif greeting.startswith("hello") == False and greeting.startswith("h"):
     print("$20")
 else:
     print("$100")

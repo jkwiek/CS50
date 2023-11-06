abbaf5550e174acd82769fdd0879ec34a8f82742
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-06 20:35:31 +0000

    Mon, Nov 6, 2023, 2:35 PM -06:00

--- extensions.py
+++ extensions.py
@@ -1,6 +1,6 @@
 filetype = input("file name: ")
-filetype = filetype.lower().strip().split("."[name,type])
-if filetype.endswith(".gif") == True:
+filetype = filetype.lower().strip().split(".")
+if '2' == True:
     print("image/gif")
 elif filetype.endswith(".jpg") == True or filetype.endswith(".jpeg") == True:
     print("image/jpeg")

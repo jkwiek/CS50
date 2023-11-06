2994386b6eff3cecc7e6dc4a2cac072a2742c5c5
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-06 19:13:09 +0000

    Mon, Nov 6, 2023, 1:13 PM -06:00

--- extensions.py
+++ extensions.py
@@ -3,15 +3,14 @@ filetype = filetype.lower()
 if filetype.endswith(".gif") == True:
     print("image/gif")
 elif filetype.endswith(".jpg") == True or filetype.endswith(".jpeg") == True:
-     print("image/jpeg")
-#.png .pdf.txt.zip
+    print("image/jpeg")
 elif filetype.endswith(".png") == True:
     print("image/png")
 elif filetype.endswith(".pdf") == True:
     print("application/pdf")
 elif filetype.endswith(".txt") == True:
-    print("plain.text")
+    print("text/plain")
 elif filetype.endswith(".zip") == True:
     print("application/zip")
 else:
-    print
+    print("application/octet-stream")

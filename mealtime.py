92c33b35617e75223018f4e39238fea6102c3c87
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-07 17:23:43 +0000

    Tue, Nov 7, 2023, 11:23 AM -06:00

---
+++ mealtime.py
@@ -0,0 +1,11 @@
+# def main():
+    #if 7.00 <= time <= 8.00
+
+    #elif time ==
+
+    #elif time ==
+#def convert(time):
+
+hour, minute = input("time: ").split(":")
+print (hour"."(int(minute)/60))
+

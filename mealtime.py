92deea6e5d8b0b6f9b2cc374f7c82a4fc2fd13ce
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-10 04:42:03 +0000

    Thu, Nov 9, 2023, 10:42 PM -06:00

--- mealtime.py
+++ mealtime.py
@@ -1,18 +1,18 @@
 def main():
     convert()
-    if 7.00 <= time <= 8.00 == True:
+    if 7.00 <= x <= 8.00 == True:
         print ("breakfast time")
-    elif 12.00 <= time <= 13.00:
+    elif 12.00 <= x <= 13.00:
         print ("lunch time")
-    elif 18.00 <= time <= 19.00:
+    elif 18.00 <= x <= 19.00:
         print ("dinner time")
 
 def convert():
     time = input("time: ").find(" ")
     if input("time: ").find(" p.m.") == True:
         hour, minute = time.split(':').removesuffix(" p.m.")
-        if hour = int(hour) == 12:
-            hour = 12
+        if hour == "12":
+            hour = int(12)
         else:
             hour = int(hour)+12
     else:

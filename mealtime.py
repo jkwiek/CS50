2c4c38c5534fafa5e837ff1d568f558e9af11493
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-10 05:04:38 +0000

    Thu, Nov 9, 2023, 11:04 PM -06:00

--- mealtime.py
+++ mealtime.py
@@ -1,7 +1,7 @@
 def main():
     time = input("time: ")
     convert(time)
-    if 7.00 <= time <= 8.00 == True:
+    if 7.00 <= time <= 8.00:
         print ("breakfast time")
     elif 12.00 <= time <= 13.00:
         print ("lunch time")

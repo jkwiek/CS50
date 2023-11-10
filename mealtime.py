d586bf1d1ae1206e972b719b954016d85a4a9fc7
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-10 16:57:33 +0000

    Fri, Nov 10, 2023, 10:57 AM -06:00

--- mealtime.py
+++ mealtime.py
@@ -1,16 +1,16 @@
 def main():
     time = input("time: ")
     convert(time)
-    if 7.00 <= time <= 8.00:
+    if 7.00 <= convert(time) <= 8.00:
         print ("breakfast time")
-    elif 12.00 <= time <= 13.00:
+    elif 12.00 <= convert(time) <= 13.00:
         print ("lunch time")
-    elif 18.00 <= time <= 19.00:
+    elif 18.00 <= convert(time) <= 19.00:
         print ("dinner time")
 
 def convert(time):
     hour,minute = time.split(":")
-    return (int(hour) + int(minute)/60)
+    return float(int(hour) + int(minute)/60)
 
 if __name__ == "__main__":
     main()

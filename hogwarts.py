d6acb08ee2e65774183e4cad6265cd9049cb60eb
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-12 16:03:55 +0000

    Sun, Nov 12, 2023, 10:03 AM -06:00

--- hogwarts.py
+++ hogwarts.py
@@ -1,8 +1,11 @@
 students = [
-    {"name": "Hermione", "house: "Gryffindor", "patronus": "Otter"},
+    {"name": "Hermione", "house": "Gryffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Gryffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Gryffindor", "patronus": "Jack Russell Terrier"},
+    {"name": "Draco", "house": "Slytherin", "patronus": None},
 ]
 
-
+l
 {
     'Hermione': "Gryffindor",
     'Harry': "Gryffindor",

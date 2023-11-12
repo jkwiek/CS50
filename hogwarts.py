2322db93fbad7d76e4b68e7732a4f14e5f709ee6
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-12 16:06:13 +0000

    Sun, Nov 12, 2023, 10:06 AM -06:00

--- hogwarts.py
+++ hogwarts.py
@@ -5,18 +5,5 @@ students = [
     {"name": "Draco", "house": "Slytherin", "patronus": None},
 ]
 
-l
-{
-    'Hermione': "Gryffindor",
-    'Harry': "Gryffindor",
-    'Ron': "Gryffindor",
-    'Draco: "Slytherin",
-}
 for student in students:
-    print(student,students[student], sep=", ")
-print(students["Hermione:])
-'Hermione', 'Harry', 'Ron', 'Draco'
-
-houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
-for i in range(len(students)):
-    print(i+1, students[i])
+    print(student["name"], student["house"], student["patronus"], sep=", ")

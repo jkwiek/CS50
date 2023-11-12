bfff6c59271cdd8da18f7106cd3ff6d8f2af95b7
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-12 15:52:17 +0000

    Sun, Nov 12, 2023, 9:52 AM -06:00

--- hogwarts.py
+++ hogwarts.py
@@ -1,4 +1,13 @@
-students = ['Hermione', 'Harry', 'Ron']
+students = {
+    'Hermione': "Gryffindor",
+    'Harry': "Gryffindor",
+    'Ron': "Gryffindor",
+    'Draco: "Slytherin",
+}
 
+print(students["Hermione:])
+'Hermione', 'Harry', 'Ron', 'Draco'
+
+houses = ["Gryffindor", "Gryffindor", "Gryffindor", "Slytherin"]
 for i in range(len(students)):
     print(i+1, students[i])

1bcdf27c373b57416df17f851e42cf1958b917ce
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-14 17:20:47 +0000

    Tue, Nov 14, 2023, 11:20 AM -06:00

--- twttr.py
+++ twttr.py
@@ -2,7 +2,5 @@ input = input("Input: ")
 print("Output: ", end="")
 vowels = ["a","e","i","o","u"]
 for character in input:
-    if character in vowels:
-        print("", end= "")
     if character not in vowels:
         print(character, end= "")

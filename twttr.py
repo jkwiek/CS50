48097d18bcdd1a6037aaabf492c62a6c3c795aef
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-14 03:31:34 +0000

    Mon, Nov 13, 2023, 9:31 PM -06:00

--- twttr.py
+++ twttr.py
@@ -1,7 +1,8 @@
 input = input("Input: ")
+print("Output: ")
 for character in input:
     if character == "a" or "e" or "i" or "o" or "u":
-        print()
+        print("", end="")
     else:
         character = character
-    print(character, end="")
+        print(character, end="")

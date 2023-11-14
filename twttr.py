83dc603685b4e92a4c520db02849b9d1e027f5b0
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-14 03:45:56 +0000

    Mon, Nov 13, 2023, 9:45 PM -06:00

--- twttr.py
+++ twttr.py
@@ -2,7 +2,8 @@ input = input("Input: ")
 print("Output: ", end="")
 
 for character in input:
-    if character == "a","e","i","o","u":
-        print("", end="", sep="")
-    else:
-        print(character, end="", sep="")
+    match character:
+        case "a" | "e" | "i" | "o" | "u":
+            print("", end= "" sep="")
+        case _:
+            print(character, sep="", end="")

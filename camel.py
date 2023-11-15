6379cc6a1ac89ab90bdeacb22a665557e5a15aee
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-15 03:08:09 +0000

    Tue, Nov 14, 2023, 9:08 PM -06:00

--- camel.py
+++ camel.py
@@ -1,4 +1,7 @@
 variablename = input("camelCase: ")
 print("snake_case: ")
 for char in variablename:
-    if char = 
+    if char.isupper():
+        print("_", char.lower(), end="")
+    else:
+        print(char, end="")

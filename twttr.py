27eb0cab8baba36cdb64e1dc7adeb0f246916958
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-15 03:19:50 +0000

    Tue, Nov 14, 2023, 9:19 PM -06:00

--- twttr.py
+++ twttr.py
@@ -1,3 +1,8 @@
 input = input("Input: ")
-for char in input
-    if char
+vowels = ["a","e","i","o","u"]
+print("Output: ", end="")
+for char in input:
+    if char not in vowels:
+        print(char, end="")
+print()
+

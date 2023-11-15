d17b3b1d15b33d5ebf1f4ff8cf2eba0f5c91f9c0
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-15 03:24:57 +0000

    Tue, Nov 14, 2023, 9:24 PM -06:00

--- plates.py
+++ plates.py
@@ -12,10 +12,10 @@ def main():
 
 
 def is_valid(plate):
-    if plate[:2].isalpha() and 2 <= len(plate) <= 6 and for c in plate:
+    if plate[:2].isalpha() and 2 <= len(plate) <= 6 and and for c in plate:
             if c.isdigit():
                 if plate.rpartition(c)[1].isalpha()
-                if plate.rpartition(c)
+                if plate.rpartition(c)[2].startswith(not "0")
 
     return True
 

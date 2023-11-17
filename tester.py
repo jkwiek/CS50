d9bce3f4f5d986d428209f8d364aa948f64fb986
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-17 17:30:40 +0000

    Fri, Nov 17, 2023, 11:30 AM -06:00

--- tester.py
+++ tester.py
@@ -1,9 +1,11 @@
 plate = input("Plate: ")
 for c in plate:
-        if c.isdigit():
-            numbers = plate.split(sep = c,maxsplit=1)[1]
-            if numbers.isdigit():
-                print(True)
+    if c.isdigit():
+        numbers = plate.split(sep = c,maxsplit=1)[1]
+        if numbers.isdigit() and c != "0":
+            return True
+        else:
+            return False
 
 
 #if plate.partition(c)[1].isalpha() and not plate.rpartition(c)[2].startswith("0"):

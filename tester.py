2277487449eab6e52f95f4a57b3cfc74f0847438
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-17 15:29:23 +0000

    Fri, Nov 17, 2023, 9:29 AM -06:00

--- tester.py
+++ tester.py
@@ -1,6 +1,7 @@
 plate = input("Plate: ")
 for c in plate:
         if c.isdigit():
-            print(plate.split(sep=c,maxsplit=1)[1])
+            print(plate.rsplit(sep=c,maxsplit=1)[0])
 
 #if plate.partition(c)[1].isalpha() and not plate.rpartition(c)[2].startswith("0"):
+# split at number c: after the number must be only numbers 

8586bcd7c67cb8ade1ff1694ca52589ea2968963
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-17 17:15:07 +0000

    Fri, Nov 17, 2023, 11:15 AM -06:00

--- plates.py
+++ plates.py
@@ -23,7 +23,7 @@ def numbersvalid(plate):
     for c in plate:
         if c.isdigit():
             numbers = plate.split(sep = c,maxsplit=1)[1]
-            numbers.isdigit()
+            numbers.isdigit() and c != "0"
 
 def charactersvalid(plate):
     plate.find(".", " ", ",", "!", "?") == "-1"

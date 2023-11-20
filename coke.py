04ec53a4d3a80b1076ad0f65b3e13ebebd058751
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-20 03:21:13 +0000

    Sun, Nov 19, 2023, 9:21 PM -06:00

--- coke.py
+++ coke.py
@@ -1,5 +1,10 @@
+#Suppose that a machine sells bottles of Coca-Cola (Coke) for 50 cents and only accepts
+#25 cents, 10 cents, and 5 cents.
+#insert a coin, one at a time, each time informing the user of the amount due.
+#Once the user has inputted at least 50 cents, output how many cents in change the user is owed.
+#Assume that the user will only input integers, and ignore any integer that isn’t an accepted denomination.
 print("Amount Due: 50")
 input("Insert Coin:)
 
-coin = input("Amount Due: "))
+coin = int(input("Amount Due: "))
 

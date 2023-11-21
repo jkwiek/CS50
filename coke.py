070a0d3ff4b8b0b5a55c9d7e56ea6d65c927d95c
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-21 15:53:24 +0000

    Tue, Nov 21, 2023, 9:53 AM -06:00

--- coke.py
+++ coke.py
@@ -3,6 +3,7 @@
 #insert a coin, one at a time, each time informing the user of the amount due.
 #Once the user has inputted at least 50 cents, output how many cents in change the user is owed.
 #Assume that the user will only input integers, and ignore any integer that isn’t an accepted denomination.
+coinsum=0
 while coinsum < 50:
     coin = int(input("Insert Coin: "))
     coins: [5,10,25]

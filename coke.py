39c8e0ee514b65a2e4cb9b4d1d5ec88a96a4db69
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-21 16:23:47 +0000

    Tue, Nov 21, 2023, 10:23 AM -06:00

--- coke.py
+++ coke.py
@@ -4,14 +4,15 @@
 #Once the user has inputted at least 50 cents, output how many cents in change the user is owed.
 #Assume that the user will only input integers, and ignore any integer that isn’t an accepted denomination.
 coinsum = 0
+coins = [5,10,25]
+
 while coinsum < 50:
     coin = int(input("Insert Coin: "))
-    coins = [5,10,25]
     if coin in coins:
         coinsum += coin
         remainder = 50 - coinsum
             if remainder < 0:
                 remainder = 0
         print ("Amount Due:", remainder)
-        
+
 print("Change Owed:", coinsum - 50)

fbf50173ec9ae668fb0147d70c1883a57384ce0c
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-22 04:51:03 +0000

    Tue, Nov 21, 2023, 10:51 PM -06:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,9 +1,9 @@
 while True:
     try:
         x,y = input("Fraction: ").split("/")
-        result = int(x) / int(y) *100
+        result = int(x) / int(y) * 100
     except:
         continue
     else:
         break
-print(result:.1f, "%", sep="")
+print(f"{result:1f}", "%", sep="")

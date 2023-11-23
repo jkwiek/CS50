dfc98990431514893073a17e855911ea56a52560
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-23 16:56:20 +0000

    Thu, Nov 23, 2023, 10:56 AM -06:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,14 +1,14 @@
-entrees=[
-    {"Baja Taco": 4.25},
-    {"Burrito": 7.50},
-    {"Bowl": 8.50},
-    {"Nachos": 11.00},
-    {"Quesadilla": 8.50},
-    {"Super Burrito": 8.50},
-    {"Super Quesadilla": 9.50},
-    {"Taco": 3.00},
-    {"Tortilla Salad": 8.00}
-]
+entrees={
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
 
 while True:
     try:
@@ -18,5 +18,5 @@ while True:
     except EOFError:
         break
     else:
-        print([entrees])
+        print(entrees[price])
 

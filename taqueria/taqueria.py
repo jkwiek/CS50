69841cd504b5ecc0eca5240a9b91f8d022881765
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-23 02:10:36 +0000

    Wed, Nov 22, 2023, 8:10 PM -06:00

--- taqueria/taqueria.py
+++ taqueria/taqueria.py
@@ -1,15 +1,22 @@
-{
-    "Baja Taco": 4.25,
-    "Burrito": 7.50,
-    "Bowl": 8.50,
-    "Nachos": 11.00,
-    "Quesadilla": 8.50,
-    "Super Burrito": 8.50,
-    "Super Quesadilla": 9.50,
-    "Taco": 3.00,
-    "Tortilla Salad": 8.00
-}
+entrees=[
+    {"Baja Taco": 4.25},
+    {"Burrito": 7.50},
+    {"Bowl": 8.50},
+    {"Nachos": 11.00},
+    {"Quesadilla": 8.50},
+    {"Super Burrito": 8.50},
+    {"Super Quesadilla": 9.50},
+    {"Taco": 3.00},
+    {"Tortilla Salad": 8.00}
+]
 
+while True:
 try:
-    input("")
-except 
+    input("Item: ").title() in entrees
+
+except ValueError:
+
+except EOFError:
+
+
+else:

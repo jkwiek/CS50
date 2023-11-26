c1801f2003d0e5942d2f79efb94a5213523123b2
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-26 02:04:01 +0000

    Sat, Nov 25, 2023, 8:04 PM -06:00

--- grocery/grocery.py
+++ grocery/grocery.py
@@ -1,12 +1,12 @@
+groceries={}
 while True:
     try:
         food = input()
     except EOFError:
         break
     else:
-        amount = list.count(food)
-        list ={}
-        list[amount]=food
-        print(list)
+        amount = groceries.count(food)
+        groceries[amount]=food
+        print(groceries)
 
 #capitalize, number, alphabetize, print

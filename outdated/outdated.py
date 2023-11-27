1ca396f6e8cde22c22bccf1b20494608829e8e46
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-27 22:25:34 +0000

    Mon, Nov 27, 2023, 4:25 PM -06:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -18,28 +18,23 @@ try:
     month, day, year = input().split(" ", "/")
     if month in months:
         month = int(months.index(month)) + 1
-    if 1 <= month.int() <= 12
+    if 1 <= month.int() <= 12:
         if length(month)  == 1:
             month = f"{month: 02f}
         else:
             month = month
-    if 1<= int(day) <= 31
+    if 1<= int(day) <= 31:
         if length(day)  == 1:
             day= f"{day: 02f}
         else:
             day = day
-    if length(year.int())==4
-        
-
+    if length(year.int())==4:
+        year = year
     else:
         continue
+
 except ValueError:
     countinue
 
 else:
-
-
-
-
-    if month
-print(year, month, day, sep="-")
+    print(year, month, day, sep="-")

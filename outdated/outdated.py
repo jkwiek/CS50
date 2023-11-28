6960e9716634cef0ea78fc92c5d5a728424410ce
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-28 03:38:35 +0000

    Mon, Nov 27, 2023, 9:38 PM -06:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -20,7 +20,6 @@ while True:
         elif 1 <= month.int() <= 12:
             if len(month)  == 1:
                 month = 0 + month
-
         if 1<= int(day) <= 31:
             if len(day)  == 1:
                 day = 0 + day

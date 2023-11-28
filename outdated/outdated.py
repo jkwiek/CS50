fef2431a8b4edeab410cee2f5fbfd6628eb903b6
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-28 17:04:52 +0000

    Tue, Nov 28, 2023, 11:04 AM -06:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -15,24 +15,19 @@ months= [
 def monthvalid(month):
     if month in months:
         month = int(months.index(month)) + 1
-    if month = str(month).zfill(2)
+        month = str(month).zfill(2)
     elif 1 <= int(month) <= 12:
         month = month.zfill(2)
 def dayvalid(day):
     if 1<= int(day) <= 31:
         day = day.zfill(2)
+def yearvalid(year):
+    len(year)==4
 
 while True:
     try:
         month, day, year = input("Date: ").replace(",","").replace("-"," ").split(" ")
-        if not monthvalid() or not dayvalid() or not yearvalid()
-            alphaMonth = int(months.index(month)) + 1
-            month = str(alphaMonth).zfill(2)
-        elif 1 <= int(month) <= 12:
-            month = month.zfill(2)
-        if 1<= int(day) <= 31:
-            day = day.zfill(2)
-        if len(year)!= 4:
+        if not monthvalid() or not dayvalid() or not yearvalid():
             continue
     except ValueError:
         continue

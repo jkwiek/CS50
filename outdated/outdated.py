3b7a18737b801303a98619dafdb7915de74f0710
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-28 03:36:55 +0000

    Mon, Nov 27, 2023, 9:36 PM -06:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -14,7 +14,7 @@ months= [
 ]
 while True:
     try:
-        month, day, year = input().strip(",").split(" ").split("/")
+        month, day, year = input().strip(",").split(" ")
         if month in months:
             month = int(months.index(month)) + 1
         elif 1 <= month.int() <= 12:

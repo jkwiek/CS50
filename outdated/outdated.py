213ca7429af2e70d6d8bcf7427a862e8b8ade58c
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-11-29 03:05:30 +0000

    Tue, Nov 28, 2023, 9:05 PM -06:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -21,34 +21,34 @@ def main():
                 alphamonth, day, year = date.split("/")
             elif "," in date:
                 nummonth, day, year = date.replace(",","").split(" ")
-            if None in (monthvalid(month), dayvalid(day), yearvalid(year)):
+            if None in (convert(month), convert(day), convert(year)):
                 continue
         except ValueError:
             continue
         else:
-            print(yearvalid(year), monthvalid(month), dayvalid(day), sep="-")
+            print(convert(year), convert(month), convert(day), sep="-")
             break
 
-def monthvalid(alphamonth):
+def convert(alphamonth):
     if alphamonth in months:
         month = int(months.index(alphamonth)) + 1
         month = str(alphamonth).zfill(2)
         return month
     else:
         return None
-def monthvalid(nummonth):
+def convert(nummonth):
     if 1 <= int(nummonth) <= 12:
         month = nummonth.zfill(2)
         return month
     else:
         return None
-def dayvalid(day):
+def convert(day):
     if 1<= int(day) <= 31:
         day = day.zfill(2)
         return day
     else:
         return None
-def yearvalid(year):
+def convert(year):
     if len(year)==4:
         return year
     else:

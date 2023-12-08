04cb4931b71e4b0b9e621ef1954974ae9a890980
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-12-08 23:45:04 +0000

    Fri, Dec 8, 2023, 5:45 PM -06:00

--- Week 3/grocery/grocery.py
+++ Week 3/grocery/grocery.py
@@ -1,15 +1,17 @@
 groceries={}
-while True:
-    try:
-        item = input().upper()
-    except EOFError:
-        break
-    else:
-        if item in groceries:
-            groceries[item]+=1
+def getinputs():
+    while True:
+        try:
+            item = input().upper()
+        except EOFError:
+            break
         else:
-            groceries[item]=1
-groceriesList = sorted(groceries)
-for item in groceriesList:
-    print(groceries[item], item)
+            if item in groceries:
+                groceries[item]+=1
+            else:
+                groceries[item]=1
+def sortprint(groceries):
+    groceriesList = sorted(groceries)
+    for item in groceriesList:
+        print(groceries[item], item)
 

15d0435b0dd79606a4925fbef5e1ddc4f0b3cb59
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-12-09 04:05:38 +0000

    Fri, Dec 8, 2023, 10:05 PM -06:00

--- adieu/adieu.py
+++ adieu/adieu.py
@@ -2,12 +2,10 @@ Names=[]
 while True:
         try:
             Name = input("Name: ")
-            continue
         except EOFError:
             break
         else:
-            Names =+ Name
-Names= p.join(Names)
+            Names = Name.append()
 print("Adieu, adieu, to", Names)
 
 

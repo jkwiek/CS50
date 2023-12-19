8d5781c869a34ef14a8ac91ab2ed2db202b0db43
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-12-19 18:22:54 +0000

    Tue, Dec 19, 2023, 12:22 PM -06:00

--- game/game.py
+++ game/game.py
@@ -2,12 +2,16 @@ import random
 n = int(input("Level: "))
 goal = random.randint(1,n)
 while True:
-    guess = int(input("Guess: "))
-    if guess < goal:
-        print("Too small!")
-    elif guess > goal:
-        print("Too large!")
-    else:
-        break
+    try:
+        guess = int(input("Guess: "))
+        if guess < goal:
+            print("Too small!")
+        elif guess > goal:
+            print("Too large!")
+        else:
+            break
+    except ValueError:
+            continue
+    
 print("Just right!")
 

61c7e95f28c7135250e5f4ab2bb8393a6fd55d43
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-12-20 17:08:53 +0000

    Wed, Dec 20, 2023, 11:08 AM -06:00

---
+++ professor/professor.py
@@ -0,0 +1,19 @@
+import random
+
+def main():
+    get_level()
+
+
+def get_level():
+    while True:
+        try:
+            level = int(input: )
+    return level
+
+
+def generate_integer(level):
+    ...
+
+
+if __name__ == "__main__":
+    main()

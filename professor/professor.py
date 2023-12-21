6fd5c2b50cc2dbf1ebfb0ccdebb58f6ce5919e47
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-12-21 17:41:18 +0000

    Thu, Dec 21, 2023, 11:41 AM -06:00

--- professor/professor.py
+++ professor/professor.py
@@ -2,8 +2,9 @@ import random
 
 def main():
     level = get_level()
-    number = generate_integers(level)
-    print(number, "+", number)
+    number1 = generate_integers(level)
+    number2 = generate_integers(level)
+    print(number1, "+", number2)
     #for problems <= 10:
         #number= generate_integers(level)
         #solution = number + generate_integers(level)
@@ -22,8 +23,10 @@ def get_level():
 
 def generate_integers(level):
     integer= []
+    digit = str(random.randint(1,9))
+        integer += [digit]
     for _ in range(level):
-        digit = str(random.randint(0,9))
+        digit = str(random.randint(1,9))
         integer += [digit]
     integer = "".join(integer)
     return integer

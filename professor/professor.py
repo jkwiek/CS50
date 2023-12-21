fae47e96ba15b148f766f154fd63534572b1edbf
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-12-21 17:14:45 +0000

    Thu, Dec 21, 2023, 11:14 AM -06:00

--- professor/professor.py
+++ professor/professor.py
@@ -22,14 +22,13 @@ def get_level():
 
 def generate_integers(level):
     integer= []
-    n==.0
+    n=0
     while n<2:
         for _ in range(level):
             digit = str(random.randint(0,9))
             integer += [digit]
         n=+1
     integer = "".join(integer)
-
 
 if __name__ == "__main__":
     main()

026b76a47146bde66290faec728ddf324f7dfae2
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-12-30 03:18:46 +0000

    Fri, Dec 29, 2023, 9:18 PM -06:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -3,6 +3,7 @@ import requests
 while True:
     try:
         input = float(sys.argv[1])
+        requests.get
     except ValueError:
         print("Command-line argument is not a number")
         break

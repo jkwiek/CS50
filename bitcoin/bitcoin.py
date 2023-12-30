f821b052f2babbba717f1075fc9ad0b8073233b6
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-12-30 04:11:21 +0000

    Fri, Dec 29, 2023, 10:11 PM -06:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -4,7 +4,8 @@ import requests
 
 response = requests.get(" https://api.coindesk.com/v1/bpi/currentprice.json")
 response = response.json()
-conversion_factor = response["rate_float"]
+conversion_factor = response["bpi"["rate_float"]]
+
 
 n = float(sys.argv[1])
 

dd617cef735e3f268b7d979d261fbfaea184ac08
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-12-30 04:04:24 +0000

    Fri, Dec 29, 2023, 10:04 PM -06:00

--- bitcoin/bitcoin.py
+++ bitcoin/bitcoin.py
@@ -6,16 +6,14 @@ response = requests.get(" https://api.coindesk.com/v1/bpi/currentprice.json")
 response = response.json()
 conversion_factor = response["rate_float"]
 
-while True:
-    try:
-        USD = float(sys.argv[1])
+n = float(sys.argv[1])
     except ValueError:
         print("Command-line argument is not a number")
         break
     except IndexError:
         print("Missing command-line argument")
         break
-bitcoin
+USD
 #except requests.RequestException:
 
 

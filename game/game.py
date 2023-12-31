c70d63d6fdc25983164a7f3266fe274f87b88112
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-12-31 16:34:24 +0000

    Sun, Dec 31, 2023, 10:34 AM -06:00

--- game/game.py
+++ game/game.py
@@ -3,20 +3,21 @@ import random
 def validate(input):
     try:
         input = int(input)
+        return input
     except ValueError:
-        return False
+        return None
 while level = None:
     level = input("Level: ")
     validate(level)
-    if level <=0:
-        continue
-    else:
-        goal = random.randint(1,level)
+    if level > 0:
+        break
+goal = random.randint(1,level)
 
-    try:
-        guess = int(input("Guess: "))
-        if not 1<=guess<=level:
-            continue
+while guess = None:
+    guess = input("Guess: ")
+    validate(guess)
+    if 1<=guess<=level:
+        break
         elif guess < goal:
             print("Too small!")
         elif guess > goal:

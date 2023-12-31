2cdbff2b159d0c2389be48324492f4289f22052b
jkwiek <149102493+jkwiek@users.noreply.github.com>
2023-12-31 15:42:16 +0000

    Sun, Dec 31, 2023, 9:42 AM -06:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -5,8 +5,9 @@ import random
 fonts = [pyfiglet.figlet]
 
 if len(sys.argv) == 1:
-    print("random")
-    #font = random.
+    font = random.choice(fonts)
+    raw_text = input("Input: ")
+    figlet_text = pyfiglet(raw_text)
 elif len(sys.argv) == 3:
     if sys.argv[1] != "-f" or "--font" or sys.argv[2] not in fonts:
         sys.exit

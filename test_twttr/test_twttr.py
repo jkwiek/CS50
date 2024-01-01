14b216047d6103c2a04a69435a1dd2807ec0e7a9
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-01 20:53:49 +0000

    Mon, Jan 1, 2024, 2:53 PM -06:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,10 +1,16 @@
 from twttr import shorten
 
-def test_allLower:
-    at
+def test_allLower():
+    assert shorten("monday") == "mndy"
+    assert shorten("tuesday") == "tsdy"
 
-def test_allUpper
+def test_allUpper():
+    assert shorten("WEDNESDAY") == "WDNSDY"
+    assert shorten("THURSDAY") == "THRSDY"
 
-def test_numbers
+def test_numbers():
+    assert shorten("12345") == "12345"
 
-def test_mix
+def test_mix():
+    assert shorten("Cs50") == "Cs50"
+    assert shorten("Friday13") == "Frdy13"

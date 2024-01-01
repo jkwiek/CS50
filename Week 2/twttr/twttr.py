20f354b2c037f5dcda7a1dbc4179fdbf9a715538
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-01 20:37:06 +0000

    Mon, Jan 1, 2024, 2:37 PM -06:00

--- Week 2/twttr/twttr.py
+++ Week 2/twttr/twttr.py
@@ -2,7 +2,7 @@ input = input("Input: ")
 vowels = ["a","e","i","o","u"]
 print("Output: ", end="")
 for char in input:
-    if char not in vowels:
+    if char.lower() not in vowels:
         print(char, end="")
 print()
 

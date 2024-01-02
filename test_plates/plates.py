49d7ecaa5decd4f3d463fe77a7b8647ee6b17491
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-02 03:14:20 +0000

    Mon, Jan 1, 2024, 9:14 PM -06:00

--- test_plates/plates.py
+++ test_plates/plates.py
@@ -10,14 +10,14 @@ def is_valid(s):
         return False
     elif not s[:2].isalpha():
         return False
-    elif for c in s:
+    for c in s:
         if c.isdigit():
             numbers = s.split(sep = c,maxsplit=1)[1]
             if not numbers.isdigit() and c == "0":
                 return False
-        elif not plate.isalpha():
+        elif not s.isalpha():
             return False
-    elif not plate.isalnum():
+    elif not s.isalnum():
         return False
     else:
         return True

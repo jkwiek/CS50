7c46efd9f43daedaa50bc46a5b0fac1b48dc2539
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-02 04:56:22 +0000

    Mon, Jan 1, 2024, 10:56 PM -06:00

--- Week 5/test_fuel/fuel.py
+++ Week 5/test_fuel/fuel.py
@@ -1,17 +1,20 @@
 def main():
-    fraction = input("Fraction: ")
-    percentage = convert(fraction)
-    print(gauge(percentage))
+    while True:
+        try:
+            fraction = input("Fraction: ")
+            percentage = convert(fraction)
+        except (ValueError, ZeroDivisionError):
+            continue
+        else:
+            print(gauge(percentage))
 
 def convert(fraction):
-        try:
-            x,y = fraction.split("/")
-            percentage = int(x) / int(y) * 100
+        x,y = fraction.split("/")
+        percentage = int(x) / int(y) * 100
             percentage = int(round(percentage))
             if percentage <0 or percentage >100:
                 raise ValueError
-        except (ValueError, ZeroDivisionError):
-            
+
         else:
             return percentage
 

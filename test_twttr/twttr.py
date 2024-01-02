4009d6ff6d9a344d9d2df09789a97248f6a48169
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-02 02:23:02 +0000

    Mon, Jan 1, 2024, 8:23 PM -06:00

--- test_twttr/twttr.py
+++ test_twttr/twttr.py
@@ -6,7 +6,7 @@ def main():
 def shorten(word):
     vowels = ["a","e","i","o","u"]
     for char in word:
-        if char.lower() in vowels:
+        if char in vowels:
             word = word.replace(char, "")
     return word
 

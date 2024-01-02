47607482e94891c7023edd1cec50abd1e20b1621
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-02 15:37:10 +0000

    Tue, Jan 2, 2024, 9:37 AM -06:00

--- Week5/test_fuel/test_fuel.py
+++ Week5/test_fuel/test_fuel.py
@@ -3,6 +3,10 @@ import pytest
 
 def test_gauge():
     assert gauge(0.5) == "E"
+    assert gauge(1) == "E"
+    assert gauge(99.5) == "F"
+    assert gauge(99) == "F"
+    assert gauge(50) == "50%"
 
 
 

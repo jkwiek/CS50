9a32fade8a034928e3ef2a56a60f0eb63d638699
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-02 03:09:49 +0000

    Mon, Jan 1, 2024, 9:09 PM -06:00

--- test_plates/plates.py
+++ test_plates/plates.py
@@ -1,12 +1,30 @@
 def main():
-
+    plate = input("Plate: ")
+    if is_valid(plate):
+        print("Valid")
+    else:
+        print("Invalid")
 
 def is_valid(s):
-
+    if not 2 <= len(plate) <= 6:
+        return False
+    elif not plate[:2].isalpha():
+        return False
+    elif for c in plate:
+        if c.isdigit():
+            numbers = plate.split(sep = c,maxsplit=1)[1]
+            if not numbers.isdigit() and c == "0":
+                return False
+        if not plate.isalpha():
+            return False
+    elif not plate.isalnum():
+        return False
+    else:
+        return True
 
 if __name__ == "__main__":
     main()
-    
+
 def main():
     plate = input("Plate: ")
     if is_valid(plate):

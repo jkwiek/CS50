670eb321cfecbee9dafed08fa612bb25358e623d
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-02 03:26:10 +0000

    Mon, Jan 1, 2024, 9:26 PM -06:00

--- test_plates/plates.py
+++ test_plates/plates.py
@@ -14,13 +14,11 @@ def is_valid(s):
         return False
     for c in s:
         if c.isdigit():
-        if c == "0":
-            return False
-        numbers = s.split(sep = c)[1]
+            if c == "0":
+                return False
+            numbers = s.split(sep = c)[1]
             if not numbers.isdigit():
                 return False
-        elif not s.isalpha():
-            return False
     else:
         return True
 

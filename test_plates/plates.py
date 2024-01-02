8b342cfb2c2dbd3342feb3baf7917ce4b3911aa1
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-02 03:12:15 +0000

    Mon, Jan 1, 2024, 9:12 PM -06:00

--- test_plates/plates.py
+++ test_plates/plates.py
@@ -6,16 +6,16 @@ def main():
         print("Invalid")
 
 def is_valid(s):
-    if not 2 <= len(plate) <= 6:
+    if not 2 <= len(s) <= 6:
         return False
-    elif not plate[:2].isalpha():
+    elif not s[:2].isalpha():
         return False
-    elif for c in plate:
+    elif for c in s:
         if c.isdigit():
-            numbers = plate.split(sep = c,maxsplit=1)[1]
+            numbers = s.split(sep = c,maxsplit=1)[1]
             if not numbers.isdigit() and c == "0":
                 return False
-        if not plate.isalpha():
+        elif not plate.isalpha():
             return False
     elif not plate.isalnum():
         return False
@@ -25,35 +25,4 @@ def is_valid(s):
 if __name__ == "__main__":
     main()
 
-def main():
-    plate = input("Plate: ")
-    if is_valid(plate):
-        print("Valid")
-    else:
-        print("Invalid")
 
-def is_valid(plate):
-    if lengthvalid(plate) and beginningvalid(plate) and numbersvalid(plate) and charactersvalid(plate):
-        return True
-
-def lengthvalid(plate):
-    if 2 <= len(plate) <= 6:
-        return True
-
-def beginningvalid(plate):
-    if plate[:2].isalpha():
-        return True
-
-def numbersvalid(plate):
-    for c in plate:
-        if c.isdigit():
-            numbers = plate.split(sep = c,maxsplit=1)[1]
-            if numbers.isdigit() and c != "0":
-                return True
-        if plate.isalpha():
-            return True
-
-def charactersvalid(plate):
-    if plate.isalnum():
-        return True
-main()

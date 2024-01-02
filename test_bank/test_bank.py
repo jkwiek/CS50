f1215e1b9aabc89a330cf09b9e61345160d43948
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-02 02:54:20 +0000

    Mon, Jan 1, 2024, 8:54 PM -06:00

--- test_bank/test_bank.py
+++ test_bank/test_bank.py
@@ -2,10 +2,9 @@ from bank import value
 
 def test_hello():
     assert value("hello") == 0
-    assert value("HeLlO") == 0
 def test_h():
     assert value("hey") == 20
-    assert value("HOWDY") == 20
+    assert value("howdy") == 20
     assert value("how's it going?") == 20
 def test_other():
     assert value("good evening") == 100

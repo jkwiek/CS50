aa58eb40f8e155f68d057aaa2aec09296940261e
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-02 04:06:33 +0000

    Mon, Jan 1, 2024, 10:06 PM -06:00

--- Week 3/fuel/fuel.py
+++ Week 3/fuel/fuel.py
@@ -1,21 +1,30 @@
-while True:
-    try:
-        x,y = input("Fraction: ").split("/")
-        result = int(x) / int(y) * 100
-    except(ValueError, ZeroDivisionError):
-        continue
-    if result <0 or result >100:
-        continue
+def main():
+    fraction = input("Fraction: ")
+    percentage = convert(fraction)
+    print(gauge(percentage))
+    
+def convert(fraction):
+    while True:
+        try:
+            x,y = fraction.split("/")
+            percentage = int(x) / int(y) * 100
+        except (ValueError, ZeroDivisionError):
+            continue
+        if percentage <0 or percentage >100:
+            continue
+        else:
+            return percentage
+
+def gauge(percentage):
+    if percentage <= 1:
+        return "E"
+
+    elif percentage >=99:
+        return "F"
+
     else:
-        break
+        percentage = round(percentage)
+        return f"{percentage}%"
 
-if result <= 1:
-    print("E")
-
-elif result >=99:
-    print("F")
-
-else:
-    result = round(result)
-    print(result, "%", sep="")
-
+if __name__ == "__main__":
+    main()

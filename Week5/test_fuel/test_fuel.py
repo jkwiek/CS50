abf12c42e11bda3ac6fbab74dfbcac3ddba0875c
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-02 15:18:18 +0000

    Tue, Jan 2, 2024, 9:18 AM -06:00

--- Week5/test_fuel/test_fuel.py
+++ Week5/test_fuel/test_fuel.py
@@ -4,6 +4,7 @@ import pytest
 def test_convert():
     assert convert("1/2") == 50
     assert convert("1/3") == 33
+    assert convert("-1/5") == -20
     with pytest.raises(ValueError):
         convert("5/2")
         convert("3.4/5")
@@ -16,6 +17,7 @@ def test_gauge():
     assert gauge("100") == "F"
     assert gauge("99") == "F"
     assert gauge("50") == "50%"
+    assert gauge("-20") == "E"
 
 
 

6e3c9845d129f7ad516762dcabfba216db02d77d
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-02 15:33:50 +0000

    Tue, Jan 2, 2024, 9:33 AM -06:00

--- Week5/test_fuel/test_fuel.py
+++ Week5/test_fuel/test_fuel.py
@@ -1,9 +1,6 @@
 from fuel import convert, gauge
 import pytest
 
-def test_convert():
-    assert convert("1/2") == 50
-
 def test_gauge():
     assert gauge("1") == "E"
     assert gauge("99") == "F"

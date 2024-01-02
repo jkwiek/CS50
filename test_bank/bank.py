c88d697adbfc97e2cbf710c7e43efdd835c89a08
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-02 02:41:35 +0000

    Mon, Jan 1, 2024, 8:41 PM -06:00

---
+++ test_bank/bank.py
@@ -0,0 +1,15 @@
+def main():
+    greeting = input("Greeting: ").strip().lower()
+    value = value(greeting)
+    print(value)
+def value(greeting):
+    if greeting.startswith("hello") == True:
+        return "$0"
+    elif greeting.startswith("h"):
+        return "$20"
+    else:
+        return "$100"
+
+if __name__ == "__main__":
+    main()
+

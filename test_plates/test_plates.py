59d38a8037afef68122d9ac5984da05fec491c27
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-02 03:47:28 +0000

    Mon, Jan 1, 2024, 9:47 PM -06:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -1,10 +1,17 @@
-from plates import isvalid
+from plates import is_valid
 
 def test_length():
-    assert isvalid(length)
-    
+    assert is_valid("CAT") == True
+    assert is_valid("C") == False
+    assert is_valid("caterpillar") == False
 def test_beginning():
-
+    assert is_valid("AB12") == True
+    assert is_valid("C139") == False
+    assert is_valid("123456") == False
 def test_numbers():
-
+    assert is_valid("CS50") == True
+    assert is_valid("CS05") == False
+    assert is_valid("AB12CD") == False
 def test_characters():
+    assert is_valid("HI!!") == False
+    assert is_valid(".?!:") == False

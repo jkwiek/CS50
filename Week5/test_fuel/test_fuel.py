bec0234de5416527f0cb304c04ebcb9f2f3045ff
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-02 15:20:17 +0000

    Tue, Jan 2, 2024, 9:20 AM -06:00

--- Week5/test_fuel/test_fuel.py
+++ Week5/test_fuel/test_fuel.py
@@ -5,10 +5,14 @@ def test_convert():
     assert convert("1/2") == 50
     assert convert("1/3") == 33
     assert convert("-1/5") == -20
+def test_convert_valueError():
+    
     with pytest.raises(ValueError):
         convert("5/2")
         convert("3.4/5")
         convert("1/cat")
+def test_convert_ZeroError():
+
     with pytest.raises(ZeroDivisionError):
         convert("1/0")
 def test_gauge():

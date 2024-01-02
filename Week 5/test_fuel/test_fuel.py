fe1b5c5846caf16f840be121c89939315ee6830e
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-02 04:16:43 +0000

    Mon, Jan 1, 2024, 10:16 PM -06:00

--- Week 5/test_fuel/test_fuel.py
+++ Week 5/test_fuel/test_fuel.py
@@ -1,10 +1,18 @@
 from fuel import convert, gauge
 
 def test_convert():
-
+    assert convert("1/2") == "50"
+    assert convert("1/3") == "33"
+    convert("5/2") raises ValueError
+    convert("3.4/5") raises ValueError
+    convert("1/cat") raises ValueError
+    convert("1/0") raises ZeroDivisionError
 def test_gauge():
-    assert
-
+    assert gauge("0") == "E"
+    assert gauge("1") == "E"
+    assert gauge("100") == "F"
+    assert gauge("99") == "F"
+    assert gauge("50") == "50%"
 
 #convert expects a str in X/Y format as input, wherein each of X and Y is an integer, and returns that fraction as a percentage rounded to the nearest int between 0 and 100, inclusive. If X and/or Y is not an integer, or if X is greater than Y, then convert should raise a ValueError. If Y is 0, then convert should raise a ZeroDivisionError.
 #gauge expects an int and returns a str that is:

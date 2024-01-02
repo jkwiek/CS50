f646ab1f5c4e492607b4605e1c40a604d7aadcac
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-02 15:28:31 +0000

    Tue, Jan 2, 2024, 9:28 AM -06:00

--- Week5/test_fuel/test_fuel.py
+++ Week5/test_fuel/test_fuel.py
@@ -6,9 +6,7 @@ def test_convert():
     assert convert("1/3") == 33
 
 def test_gauge():
-    assert gauge("0") == "E"
     assert gauge("1") == "E"
-    assert gauge("100") == "F"
     assert gauge("99") == "F"
     assert gauge("50") == "50%"
 

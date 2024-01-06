6cb0a7acf2a14b551170459ac6638b79d963a464
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-06 22:59:41 +0000

    Sat, Jan 6, 2024, 4:59 PM -06:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -7,7 +7,9 @@ if len(sys.argv) == 2:
         sys.exit("Not a CSV file")
     try:
         with open(request) as file:
-            table = []
+            table = {}
+            for line in file:
+                
             print(tabulate(file,headers="firstrow",tablefmt="grid"))
     except FileNotFoundError:
         sys.exit("File does not exist")

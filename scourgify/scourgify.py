d91b183131622e4189baeb5f487b51de0dac62d6
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-06 23:29:43 +0000

    Sat, Jan 6, 2024, 5:29 PM -06:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -1,1 +1,21 @@
 import sys
+
+
+if len(sys.argv) == 2:
+    request = sys.argv[1]
+    if not request.endswith(".csv"):
+        sys.exit("Not a CSV file")
+    try:
+        with open(request) as file:
+            table = []
+            for line in file:
+                row = line.rstrip().split(",")
+                table.append(row)
+            print(tabulate(table,headers="firstrow",tablefmt="grid"))
+    except FileNotFoundError:
+        sys.exit("File does not exist")
+
+elif len(sys.argv) == 1:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")

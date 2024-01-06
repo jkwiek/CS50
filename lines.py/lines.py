8ad753431ba0710759054648aa9b6525bd72c087
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-06 03:28:09 +0000

    Fri, Jan 5, 2024, 9:28 PM -06:00

--- lines.py/lines.py
+++ lines.py/lines.py
@@ -5,6 +5,9 @@ if len(sys.argv) == 2:
         file = open(request)
     except FileNotFoundError:
         sys.exit("File does not exist")
+    else:
+        if file.split(".")[1] =! "py":
+            sys.exit("F)
 elif len(sys.argv) == 1:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 2:

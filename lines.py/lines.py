4a706d3e9e2a6d9ca8d1ca1d4b48f1abaf43d563
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-06 04:24:13 +0000

    Fri, Jan 5, 2024, 10:24 PM -06:00

--- lines.py/lines.py
+++ lines.py/lines.py
@@ -1,23 +1,27 @@
 import sys
-if len(sys.argv) == 2:
-    request = sys.argv[1]
-    if request.split(".")[1] != "py":
-            sys.exit("Not a Python file")
-    try:
-        file = open(request)
-        lines = []
-    except FileNotFoundError:
-        sys.exit("File does not exist")
-    else:
-            #figure out a way to make the file be found not just within the folder but generally
-        with open(request) as file:
-            for line in file:
-                if not line.lstrip().startswith("#" or ""):
-                    lines.append(line)
-        print(len(lines))
+def main():
+    if len(sys.argv) == 2:
+        request = sys.argv[1]
+        if request.split(".")[1] != "py":
+                sys.exit("Not a Python file")
+        try:
+            file = open(request)
+            lines = []
+        except FileNotFoundError:
+            sys.exit("File does not exist")
+        else:
+                #figure out a way to make the file be found not just within the folder but generally
+            with open(request) as file:
+                for line in file:
+                    if not line.lstrip().startswith("#" or " "):
+                        lines.append(line)
+            print(len(lines))
 
-elif len(sys.argv) == 1:
-    sys.exit("Too few command-line arguments")
-elif len(sys.argv) > 2:
-    sys.exit("Too many command-line arguments")
+    elif len(sys.argv) == 1:
+        sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
 
+
+if __name__ == "__main__":
+    main()

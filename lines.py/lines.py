d93c80c0d2cdd464c8eb4a77d802d96b9e1a43f5
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-06 03:21:40 +0000

    Fri, Jan 5, 2024, 9:21 PM -06:00

--- lines.py/lines.py
+++ lines.py/lines.py
@@ -1,5 +1,12 @@
 import sys
+if len(sys.argv) == 2:
+    request = sys.argv[1]
+    try:
+        file = open(request)
+    except ValueError:
+        sys.exit
+elif len(sys.argv) == 1:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
 
-try:
-    file = sys.argv[1]
-except 

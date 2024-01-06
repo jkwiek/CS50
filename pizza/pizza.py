200dd2c1ec74a253f9da33de3c65ed5a01d8d9b3
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-06 22:35:15 +0000

    Sat, Jan 6, 2024, 4:35 PM -06:00

--- pizza/pizza.py
+++ pizza/pizza.py
@@ -1,3 +1,19 @@
+import sys
 
-with open(input) as file
-tabulate(table, headers =, tablefmt="grid")
+if len(sys.argv) == 2:
+    request = sys.argv[1]
+    if request.split(".")[1] != "csv":
+        sys.exit("Not a Python file")
+    try:
+        with open(input) as file:
+            print(tabulate(file, tablefmt="grid"))
+    except FileNotFoundError:
+        sys.exit("File does not exist")
+
+elif len(sys.argv) == 1:
+    sys.exit("Too few command-line arguments")
+elif len(sys.argv) > 2:
+    sys.exit("Too many command-line arguments")
+
+
+

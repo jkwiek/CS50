ce6612c689ec92305d832e5638873b33765f5637
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-06 03:39:47 +0000

    Fri, Jan 5, 2024, 9:39 PM -06:00

--- lines.py/lines.py
+++ lines.py/lines.py
@@ -1,22 +1,20 @@
 import sys
 if len(sys.argv) == 2:
     request = sys.argv[1]
+    if request.split(".")[1] != "py":
+            sys.exit("Not a Python file")
     try:
         file = open(request)
         lines = []
     except FileNotFoundError:
         sys.exit("File does not exist")
     else:
-        if file_name.split(".")[1] != "py":
-            sys.exit("Not a Python file")
-        else:
             #figure out a way to make the file be found not just within the folder but generally
-            with open(request) as file:
-                for line in file:
-                    if not line.lstrip().startswith("#"):
-                        lines.append(line)
-
-            print(len(lines))
+        with open(request) as file:
+            for line in file:
+                if not line.lstrip().startswith("#"):
+                    lines.append(line)
+        print(len(lines))
 
 elif len(sys.argv) == 1:
     sys.exit("Too few command-line arguments")

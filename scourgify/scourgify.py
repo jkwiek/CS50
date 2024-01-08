8d5a90070721d30d8dbe3810222ba90925372d66
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-08 17:09:49 +0000

    Mon, Jan 8, 2024, 11:09 AM -06:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -3,9 +3,11 @@ import sys
 
 if len(sys.argv) == 2:
     input, output = sys.argv[1][2]
+    if not request.endswith(".csv"):
+        sys.exit("Not a CSV file")
     try:
         with open(input) as file:
-            
+
     except FileNotFoundError:
         sys.exit("File does not exist")
 

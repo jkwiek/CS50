ff6c060031105c77d3cabe23df8edb2f0b29c6c9
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-12 23:21:37 +0000

    Fri, Jan 12, 2024, 5:21 PM -06:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -7,7 +7,7 @@ if len(sys.argv) == 3:
         with open(input) as file:
             students = []
             for line in file:
-                line = name, 
+                name = line.d
 
     except FileNotFoundError:
         sys.exit(f"Could not read {input}")

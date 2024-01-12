ba5487f8b68e9cbe165ccc5f2aa2d61309b469f5
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-12 15:08:39 +0000

    Fri, Jan 12, 2024, 9:08 AM -06:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -5,12 +5,15 @@ if len(sys.argv) == 3:
     input, output = sys.argv[1], sys.argv[2]
     try:
         with open(input) as file:
-            students = {}
+            students = []
             for line in file:
-                last, first = students["name"].split(",")
-                print(first, last)
+                last, first, house = students["name"].split(",")
+                students.append(first, last, house)
     except FileNotFoundError:
         sys.exit(f"Could not read {input}")
+    finally:
+        with open(output, "w") as file:
+            append(students)
 
 elif len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")

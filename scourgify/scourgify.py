06d88860c5adc7d881ee0d4558861018d29917d7
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-01-12 14:57:24 +0000

    Fri, Jan 12, 2024, 8:57 AM -06:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -2,9 +2,7 @@ import sys
 
 
 if len(sys.argv) == 3:
-    input, output = sys.argv[0], sys.argv[1]
-    if not input.endswith(".csv") and not output.endswith(".csv"):
-        sys.exit("Not a CSV file")
+    input, output = sys.argv[1], sys.argv[2]
     try:
         with open(input) as file:
             students = []

7a3577d23dd9a4e687646e1142ba5adf8d9309f5
jkwiek <149102493+jkwiek@users.noreply.github.com>
2024-02-05 10:13:25 -0600

    scourgify.py

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -7,21 +7,24 @@ if len(sys.argv) == 3:
         with open(input) as file:
             reader = csv.DictReader(file, fieldnames= ["name", "house"])
             next(reader)
+            names = []
             for line in reader:
                 house = line["house"]
                 name = line["name"]
-                name = name.strip('\"')
+                name = name.strip('\"').replace(" ","")
                 last, first = name.split(",")
-                with open(output, "a") as file:
-                    write = csv.writer(file)
-                    write.writerow([first, last, house])
+                names.append({"first": first, "last": last, "house": house})
+        with open(output, "w") as file:
+            write = csv.DictWriter(file, fieldnames = ["first","last","house"])
+            write.writeheader()
+            for row in names:
+                write.writerow(row)
 
     except FileNotFoundError:
         sys.exit(f"Could not read {input}")
-
 
 elif len(sys.argv) < 3:
     sys.exit("Too few command-line arguments")
 elif len(sys.argv) > 3:
     sys.exit("Too many command-line arguments")
-
+    
